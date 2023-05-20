d661d1838b1640efca70955c7f46d8a8112e7fdc
Hawraa Atat <104064533+HawraaAtat@users.noreply.github.com>
2023-05-20 11:03:20 +0300

    fixing issues in the enrollment concept

--- app/src/main/java/com/example/elearningapp/CourseListAdapter.java
+++ app/src/main/java/com/example/elearningapp/CourseListAdapter.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.elearningapp.tutor.AddCourse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -79,7 +82,27 @@ public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.Vi
             }
         });
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(context, PlayCourse.class);
+//                intent.putExtra("tutor", modelVideo.getTutor());
+//                intent.putExtra("courseTitle", modelVideo.getTitle());
+//                context.startActivity(intent);
+//            }
+//        });
+
+        // Check if the student is enrolled in the course
+        checkEnrollmentStatus(modelVideo.getTutor(), modelVideo.getTitle(), holder);
+
+        holder.enrollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enrollInCourse(modelVideo.getTutor(), modelVideo.getTitle(), holder);
+            }
+        });
+
+        holder.watchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, PlayCourse.class);
@@ -88,25 +111,60 @@ public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.Vi
                 context.startActivity(intent);
             }
         });
-
-        holder.enrollButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                enrollInCourse(modelVideo.getTutor(), modelVideo.getTitle());
-            }
-        });
     }
 
-    private void enrollInCourse(String tutor, String courseTitle) {
+    private void checkEnrollmentStatus(String tutor, String courseTitle, ViewHolder holder) {
         String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference enrollmentRef = FirebaseDatabase.getInstance().getReference("enrollments")
                 .child(userId)
                 .child(tutor)
                 .child(courseTitle);
 
-        enrollmentRef.setValue(true);
-        // You can add a success message or perform additional actions after enrollment
-        Log.d("CourseListAdapter", "Enrollment successful");
+        enrollmentRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Student is enrolled in the course
+                    holder.enrollButton.setVisibility(View.GONE);
+                    holder.watchButton.setVisibility(View.VISIBLE);
+                } else {
+                    // Student is not enrolled in the course
+                    holder.enrollButton.setVisibility(View.VISIBLE);
+                    holder.watchButton.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error
+            }
+        });
+    }
+
+    private void enrollInCourse(String tutor, String courseTitle, ViewHolder holder) {
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference enrollmentRef = FirebaseDatabase.getInstance().getReference("enrollments")
+                .child(userId)
+                .child(tutor)
+                .child(courseTitle);
+
+        enrollmentRef.setValue(true)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Enrollment successful
+                        Toast.makeText(context, "Enrollment successful", Toast.LENGTH_SHORT).show();
+                        holder.enrollButton.setVisibility(View.GONE);
+                        holder.watchButton.setVisibility(View.VISIBLE);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Enrollment failed
+                        Toast.makeText(context, "Enrollment failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override
@@ -117,7 +175,7 @@ public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.Vi
     static class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView tvTitle, tvTime, tvTotalLessons, tvCategory, tvTutor;
-        Button enrollButton;
+        Button enrollButton, watchButton;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -128,6 +186,7 @@ public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.Vi
             tvCategory = itemView.findViewById(R.id.categoryTv);
             tvTutor = itemView.findViewById(R.id.tutorTv);
             enrollButton = itemView.findViewById(R.id.enrollButton);
+            watchButton = itemView.findViewById(R.id.watchButton);
         }
     }
 }
